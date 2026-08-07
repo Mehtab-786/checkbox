e1dc6cb24e37e6be166cdfb790c674a67accd258
Mehtab Hussain <tradercoder786@gmail.com>
2026-08-07 21:25:23 +0530

    feat: trying to  implement real-time checkbox synchronization

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import path from 'node:path';
+import { dummyData } from './db.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.resolve('./public')))
+
+app.get('/health', (req, res) => {
+    return res.json({ healthy: true, message: 'Server is healthy' })
+})
+
+app.get('/checkboxes', (req, res) => {
+    return res.json({ data: dummyData })
+})
+
 
 
 export default app
